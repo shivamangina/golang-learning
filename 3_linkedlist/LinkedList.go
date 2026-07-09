0f8dd0d90ca06c073d29dfa0d996491074136a09
Shiva Kumar Mangina <shiva2nani.mangina@gmail.com>
2026-07-09 23:50:25 +0530

    fix: Reverse a linked List

--- 3_linkedlist/LinkedList.go
+++ 3_linkedlist/LinkedList.go
@@ -11,9 +11,71 @@ func NewNode(data int) *ListNode {
 
 type LinkedList struct {
 	Head *ListNode
-	Tail *ListNode
+}
+
+func (ll *LinkedList) Append(n *ListNode) {
+	if ll.Head == nil {
+		ll.Head = n
+		return
+	}
+
+	current := ll.Head
+	for current.Next != nil {
+		current = current.Next
+	}
+	current.Next = n
+
+}
+
+func (ll *LinkedList) Preappend(n *ListNode) {
+	second := ll.Head
+	n.Next = second
+	ll.Head = n
+
+}
+
+func (ll *LinkedList) Reverse() {
+	var prev *ListNode
+	curr := ll.Head
+
+	for curr != nil {
+		next := curr.Next
+		curr.Next = prev
+		prev = curr
+		curr = next
+	}
+
+	ll.Head = prev
+}
+
+func (ll *LinkedList) Print() {
+	cur := ll.Head
+	for cur != nil {
+		println(cur.Val)
+		cur = cur.Next
+	}
+
 }
 
 func main() {
+	node1 := NewNode(1)
+	node2 := NewNode(2)
+	node3 := NewNode(5)
+	node4 := NewNode(6)
+
+	ll := LinkedList{
+		Head: nil,
+	}
+
+	ll.Append(node1)
+	ll.Append(node2)
+	ll.Append(node3)
+	ll.Preappend(node4)
+
+	ll.Print()
+
+	ll.Reverse()
+
+	ll.Print()
 
 }
